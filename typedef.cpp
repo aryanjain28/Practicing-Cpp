9036c964f049cea328643df87d5df08e35ef50aa
Aryan Jain <aryanjn807@gmail.com>
2023-03-09 14:15:38 -0500

    -> typedef

---
+++ typedef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+struct Student
+{
+    string fName;
+    string lName;
+};
+
+typedef struct
+{
+    string fName;
+    string lName;
+} Student1;
+
+int main()
+{
+    return 1;
+}
