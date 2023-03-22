b9fba6deca68697aa7e085a6f1fab60ed9803bfd
Aryan Jain <aryanjn807@gmail.com>
2023-03-22 09:53:04 -0400

    -> init stl

--- STL.cpp
+++ STL.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
+// C++ STL (Standard Template Library) is a powerful set
+// of C++ template classes to provide general-purpose classes
+// and functions with templates that implement many popular
+// and commonly used algorithms and data structures like
+// vectors, lists, queues, and stacks.
+
+// STL has: Containers. Algorithms. Iterators.
+
 int main()
 {
+    
     return 1;
 }
