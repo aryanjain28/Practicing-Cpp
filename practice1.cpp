a583a67471d351bd481770a2126294beaf19c1bd
Aryan Jain <aryanjn807@gmail.com>
2023-03-26 21:37:50 -0400

    -> more on structs

--- practice1.cpp
+++ practice1.cpp
@@ -7,6 +7,7 @@ struct Student
 {
     string name;
     int age;
+    Student *frnd;
 };
 
 int main()
@@ -15,16 +16,30 @@ int main()
     Student s2;
     Student s3;
 
+    s1.name = "Aryan";
+    s1.age = 25;
+    s1.frnd = NULL;
+
+    s2.name = "Josh";
+    s2.age = 23;
+    s2.frnd = &s1;
+
     cout << "Structures" << endl;
 
     cout << s1.name << endl;
     cout << s1.age << endl;
+    if (s1.frnd != NULL)
+    {
+        cout << "Friend: " << s1.frnd->name << endl; // error because s1 has NULL friends.
+    }
+    else
+    {
+        cout << s1.name << " has no friends." << endl;
+    }
 
     cout << s2.name << endl;
     cout << s2.age << endl;
-
-    cout << s3.name << endl;
-    cout << s3.age << endl;
+    cout << s2.frnd->name << endl; // Josh has one friend - Aryan (s1)
 
     return 1;
 }
