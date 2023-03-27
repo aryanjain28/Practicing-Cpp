99198662448e4ae7c0580d9f926031396b7cb942
Aryan Jain <aryanjn807@gmail.com>
2023-03-26 22:29:23 -0400

    -> unions

--- practice2.cpp
+++ practice2.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+union Student
+{
+    string name;
+    int age;
+};
+
 int main()
 {
+    Student s1;
+    s1.name = "Aryan";
+    s1.age = 25;
 }
