157d111ee38bd1c411352249400dfc2e9c64edcf
Aryan Jain <aryanjn807@gmail.com>
2023-04-01 14:37:59 -0400

    -> practice on use_count() in shared_pointers

--- practice5.cpp
+++ practice5.cpp
@@ -7,14 +7,49 @@ int main()
 {
     int a = 258497;
     auto sh_pointer_1 = make_shared<int>(a);
-    cout << "Number of pointers that are sharing sh_pointer_1: " << sh_pointer_1.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_1.use_count() << endl;
 
     auto sh_pointer_2 = sh_pointer_1;
 
-    cout << "Number of pointers that are sharing sh_pointer_1: " << sh_pointer_1.use_count() << endl;
-    cout << "Number of pointers that are sharing sh_pointer_2: " << sh_pointer_2.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_1.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_2.use_count() << endl;
 
     cout << "Owner before: " << sh_pointer_1.owner_before(sh_pointer_2) << endl;
+
+    cout << "sh_pointer_1 :" << *sh_pointer_1 << endl;
+    cout << "sh_pointer_2 :" << *sh_pointer_2 << endl;
+
+    auto sh_pointer_3 = sh_pointer_1;
+    auto sh_pointer_4 = sh_pointer_1;
+    auto sh_pointer_5 = sh_pointer_1;
+
+    cout << "Number of owners: " << sh_pointer_1.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_2.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_3.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_4.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_5.use_count() << endl;
+
+    cout << "----------------------------" << endl;
+
+    sh_pointer_1.reset();
+
+    cout << "Number of owners: " << sh_pointer_1.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_2.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_3.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_4.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_5.use_count() << endl;
+
+    cout << "----------------------------" << endl;
+
+    sh_pointer_1.reset(); // does nothing
+    sh_pointer_3.reset();
+    sh_pointer_4.reset();
+
+    cout << "Number of owners: " << sh_pointer_1.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_2.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_3.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_4.use_count() << endl;
+    cout << "Number of owners: " << sh_pointer_5.use_count() << endl;
 
     return 1;
 }
