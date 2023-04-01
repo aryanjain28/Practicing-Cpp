4e76e66bf779eb71ef786eaf3143784c33b71cf3
Aryan Jain <aryanjn807@gmail.com>
2023-04-01 15:51:08 -0400

    -> more on weak_ptr, shared_ptr, swap() and weak.lock()

--- practice7.cpp
+++ practice7.cpp
@@ -24,5 +24,56 @@ int main()
 
     cout << "Expired: " << boolalpha << wk_pointer.expired() << endl;
 
+    shared_ptr<int> sh_pointer_6 = make_shared<int>(15);
+    shared_ptr sh_pointer_7 = sh_pointer_6;
+    shared_ptr sh_pointer_8 = sh_pointer_6;
+
+    weak_ptr wk_ptr_6 = sh_pointer_6;
+    weak_ptr wk_ptr_7 = sh_pointer_7;
+    weak_ptr wk_ptr_8 = sh_pointer_8;
+
+    cout << "wk_ptr_6.expired(): " << wk_ptr_6.expired() << endl;
+    cout << "wk_ptr_7.expired(): " << wk_ptr_7.expired() << endl;
+    cout << "wk_ptr_8.expired(): " << wk_ptr_8.expired() << endl;
+
+    cout << "\n";
+    sh_pointer_6.reset();
+
+    cout << "wk_ptr_6.expired(): " << wk_ptr_6.expired() << endl;
+    cout << "wk_ptr_7.expired(): " << wk_ptr_7.expired() << endl;
+    cout << "wk_ptr_8.expired(): " << wk_ptr_8.expired() << endl;
+
+    cout << endl
+         << endl;
+
+    shared_ptr<int> sh_ptr_11 = make_shared<int>(11);
+    shared_ptr<int> sh_ptr_12 = make_shared<int>(12);
+
+    weak_ptr wk_ptr_11 = sh_ptr_11;
+    weak_ptr wk_ptr_12 = sh_ptr_12;
+
+    cout << "*sh_ptr_11: " << *sh_ptr_11 << endl;
+    cout << "*sh_ptr_12: " << *sh_ptr_12 << endl;
+
+    cout << "wk_ptr_11.expired(): " << wk_ptr_11.expired() << endl;
+    cout << "wk_ptr_12.expired(): " << wk_ptr_12.expired() << endl;
+
+    cout << "*wk_ptr_11.lock(): " << *wk_ptr_11.lock() << endl;
+    cout << "*wk_ptr_12.lock(): " << *wk_ptr_12.lock() << endl;
+
+    sh_ptr_11.swap(sh_ptr_12);
+    cout << endl;
+
+    cout << "*sh_ptr_11: " << *sh_ptr_11 << endl;
+    cout << "*sh_ptr_12: " << *sh_ptr_12 << endl;
+
+    cout << "wk_ptr_11.expired(): " << wk_ptr_11.expired() << endl;
+    cout << "wk_ptr_12.expired(): " << wk_ptr_12.expired() << endl;
+
+    cout << "*wk_ptr_11.lock(): " << *wk_ptr_11.lock() << endl;
+    cout << "*wk_ptr_12.lock(): " << *wk_ptr_12.lock() << endl;
+
+    cout << endl;
+
     return 1;
 }
