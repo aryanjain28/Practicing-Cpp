b62582760c5dba91f46ece35b25631249d57ec8b
Aryan Jain <aryanjn807@gmail.com>
2023-04-03 11:42:00 -0400

    -> more practice on templates

--- practice10.cpp
+++ practice10.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+// passing arr via ref - This is the syntax for
+// passing arr via reference.
+
+// (&arr)[n] tells the compiler that we want to
+// reference an array.
+
 template <typename T, typename S, size_t n>
 void preview(T (&arr)[n], S name)
 {
@@ -22,6 +28,18 @@ void preview(T (&arr)[n], S name)
     }
 }
 
+//  if "size" is not important.
+template <typename T>
+void preview2(T *arr, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << "arr[" << i << "]: " << arr[i] << endl;
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     int arr_i[] = {1, 2, 3, 4, 5};
@@ -29,6 +47,9 @@ int main()
 
     preview(arr_c, "name1");
     preview(arr_i, "name22");
+
+    preview2(arr_c, sizeof(arr_c) / sizeof(arr_c[0]));
+    preview2(arr_i, sizeof(arr_i) / sizeof(arr_i[0]));
 
     return 1;
 }
