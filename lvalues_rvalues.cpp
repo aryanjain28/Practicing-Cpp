bf4431a634e8f413b765eb983ddbcb5644898cfa
Aryan Jain <aryanjn807@gmail.com>
2023-04-05 18:08:16 -0400

    -> added more on l-value and r-value

--- lvalues_rvalues.cpp
+++ lvalues_rvalues.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int square(int);
+
 int main()
 {
     // L-value:
@@ -20,4 +22,31 @@ int main()
     // address of 15 and 25.
     // Also, a and b last longer, whereas 15 and
     // 25 are temporary.
+
+    // Eg. 2
+    int sum = a + b;
+
+    // here sum -> lvalue and expression, a + b -> rvalue
+    // we can get the memory address of sum using
+    // &sum. But we cannot get the memory address
+    // of &(a+b) -> this is temporary and throws error;
+
+    int *pointer = &sum; // no error
+
+    // Error: expression must be an lvalue or a function designator
+    // int *pointer = &(a + b) // error
+
+    // this throw error. We cannot take the address of
+    // function - square()
+    // int *pointer2 = &square(a);
+
+    // this is valid.
+    int *pointer3 = &a;
+
+    return 0;
+}
+
+int square(int x)
+{
+    return x * x;
 }
