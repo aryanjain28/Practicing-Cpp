e66a42c0c899126e63515cd8648eefabe33c6066
Aryan Jain <aryanjn807@gmail.com>
2023-04-15 12:19:27 -0400

    -> more on passing arrays using 'arr ref'

--- r_practice2.cpp
+++ r_practice2.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+template <typename T>
+size_t getWrongArrSize(T arr[])
+{
+    return sizeof(arr);
+}
+
+template <typename T, size_t n>
+int getCorrectArrSize(T (&arr)[n])
+{
+    return sizeof(arr);
+}
+
+template <typename T, size_t n>
+void print(T (&arr)[n])
+{
+    size_t size = getCorrectArrSize(arr);
+    for (size_t i = 0; i < size / sizeof(arr[0]); i++)
+    {
+        cout << "arr[" << i << "]: " << arr[i] << endl;
+    }
+}
+
 int main()
 {
     // Another interesting thing between arr and
@@ -27,6 +49,33 @@ int main()
 
     cout << "Memory address of 14: " << arr << endl;
     cout << "Memory address of 34: " << (arr + 1) << endl;
+
+    // Furthermore, there's one interesting thing I notice.
+
+    cout << "sizeof(arr): " << sizeof(arr) << endl;                   // 20 (4[int] * 5[len])
+    cout << "getWrongArrSize(arr): " << getWrongArrSize(arr) << endl; // 8 {WRONGGGGG!!! SO WRONG! OMG!}
+
+    // But why?
+    // the above function simply returns the size of the pointer.
+    // as described above 'arr' is just a pointer. Therefore,
+    // the 8 it returned is just a pointer size.
+
+    // Solution?
+    // Sending as arr-reference;
+    // SYNTAX: (&arr)[];
+    // Yeah I know!
+
+    // Now
+    cout << "arr : " << arr << endl;
+    cout << "getCorrectArrSize(arr): " << getCorrectArrSize(arr) << endl; // 20 Correct!
+
+    cout << "\n\nINT ARR: " << endl;
+    int i_arr[] = {52, 6, 45, 64, 6756};
+    print(i_arr);
+
+    cout << "\nSRING ARR: " << endl;
+    string s_arr[] = {"asd", "saf", "345", "name"};
+    print(s_arr);
 
     return 1;
 }
