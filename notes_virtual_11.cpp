ab995d201942c091339da2a19a5955851b299038
Aryan Jain <aryanjn807@gmail.com>
2023-04-16 13:03:48 -0400

    -> more on virtual

--- notes_virtual_11.cpp
+++ notes_virtual_11.cpp
@@ -3,23 +3,32 @@
 class Entity
 {
 public:
-    std::string virtual GetName() { return "Entity"; }
+    virtual std::string GetName() { return "Entity"; }
 };
 
 class Player : public Entity
 {
 public:
+    // Player(std::string name) : m_name(name) {}
     Player(const std::string &name) : m_name(name) {}
-    std::string GetName() { return m_name; }
+    std::string GetName() override { return m_name; }
 
 private:
     std::string m_name;
 };
 
+// This happends because the compiler calls the function
+// of the type provided, "Entity";
 void callingFunction(Entity *e)
 {
     std::string name = e->GetName();
     std::cout << "The name is : " << name << std::endl;
+}
+
+// obsessed with l-value and r-value!!
+void testing(const int &name)
+{
+    std::cout << name << std::endl;
 }
 
 int main()
@@ -32,4 +41,7 @@ int main()
 
     callingFunction(e);
     callingFunction(p);
+
+    int a = 1;
+    testing(a + 1);
 }
